2b2382533d5e999488d86368b2225cea060ea002
unknown <cheyenne.schwartz07@gmail.com>
2022-10-01 20:45:49 -0500

    Created mainMenu function

--- app.js
+++ app.js
@@ -14,3 +14,39 @@ function app(people) {
     }
 }
 
+function mainMenu(person, people) {
+
+    if (!person) {
+        alert("Could not find that individual.");
+        return app(people);
+    }
+
+    var displayOption = prompt("We have found " + person.firstName + " " + person.lastName + " . Do you want to know their info, family or descendants? Type your selection or cancel.");
+
+    if (displayOption === null) {
+        app(people);
+    }
+    else {
+        displayOption = displayOption.toLowerCase();
+
+        switch (displayOption) {
+            case "info":
+                displayPerson(person, people);
+                break;
+            case "family":
+                displayFamily(person, people);
+                break;
+            case "descendants":
+                displayDescendants(person, people);
+                break;
+            case "restart":
+                app(people);
+                break;
+            case "quit":
+                return;
+            default:
+                return mainMenu(person, people);
+        }
+    }
+}
+
